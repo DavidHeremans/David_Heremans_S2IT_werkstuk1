cfb8e2b2955985d16a34aafd55fe8cf10b30e59d
HEREMANS David (s) <david.heremans@dt-c205-07.local>
2018-05-07 15:32:56 +0200

    Initial Commit

---
+++ David_Heremans_S2IT_werkstuk1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  David_Heremans_S2IT_werkstuk1
+//
+//  Created by HEREMANS David (s) on 07/05/2018.
+//  Copyright © 2018 HEREMANS David (s). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
